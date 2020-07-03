fe7157bfe90aba4ed7c9ad8807cf36387601756b
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-07-03 20:59:51 +0530

    Added docs

--- scripts/contract.js
+++ scripts/contract.js
@@ -38,6 +38,18 @@ const interactions = async () =>{
     //The final param , event, comes with the transaction data
     //In this case, because only I want to listen to only 1 event, I have used .once
     //To listen to multiple events, use .on instead of .once
+    // blockNumber, blockHash, transactionHash – The Block and Transaction of the Log
+    // address – The contract address for the Log
+    // data – The Log data
+    // topics – An array of the Log topics
+    // args – An array of the parsed arguments for the event
+    // event – the name of the event (e.g. “Transfer”)
+    // eventSignature – the full signature of the event (e.g. “Transfer(address,address,uint256)”)
+    // getBlock() – A function that resolves to the Block containing the Log
+    // getTransaction() – A function that resolves to the Transaction containing the Log
+    // getTransactionReceipt() – A function that resolves to the Transaction Receipt containing the Log
+    // removeListener() – A function that removes this callback as a listener
+    // decode(data, topics) – A function that decodes data and topics into parsed arguments
     contract.once("countIncremented",(count,sender,event)=>{
         console.log("Count was incremented by :",sender," Count is now:",ethers.utils.formatEther(count)*(1e18))
     })
